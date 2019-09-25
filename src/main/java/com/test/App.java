48cc2042fc297754e81aa22acfade028c4a44973
tranglezyx <407833@qq.com>
2019-09-25 15:16:57 +0800

    test

--- src/main/java/com/test/App.java
+++ src/main/java/com/test/App.java
@@ -2,13 +2,9 @@ package com.test;
 
 
 import com.test.util.CommUtils;
-import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
 
 import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
-import java.util.*;
-import java.util.stream.Collectors;
 
 /**
  * @author trangle
@@ -20,8 +16,7 @@ public class App {
         System.out.println(CommUtils.encoderPasswordByECP("ADMIN","111111"));
         System.out.println(CommUtils.sha256("CRCLDAP+ADMIN+" + str));
         System.out.println("CB179A32B377E2251BD507F1A90D7CF38C96FD602C478DC90B739868210E9B43");
-//        System.out.println(Base64.getEncoder().encodeToString("CRC1234".getBytes()));
-//        System.out.println(new String(Base64.getDecoder().decode("Q1JDMTIzNA==")));
+
     }
 
 

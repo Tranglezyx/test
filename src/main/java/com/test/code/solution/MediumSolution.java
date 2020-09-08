5fd3178bea5cbf7bd2909b5c73af1393f432646b
Trangle <407833@qq.com>
2020-09-09 00:15:07 +0800

    add

--- src/main/java/com/test/code/solution/MediumSolution.java
+++ src/main/java/com/test/code/solution/MediumSolution.java
@@ -1,9 +1,36 @@
 package com.test.code.solution;
 
+import com.test.code.entity.ListNode;
+
 import java.util.HashSet;
 import java.util.Set;
 
 public class MediumSolution {
+
+    /**
+     * 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+     * <p>
+     * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+     * <p>
+     *  
+     * <p>
+     * 示例:
+     * <p>
+     * 给定 1->2->3->4, 你应该返回 2->1->4->3.
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/swap-nodes-in-pairs
+     *
+     * @param head
+     * @return
+     */
+    public static ListNode swapPairs(ListNode head) {
+        while (head.next != null) {
+
+        }
+        head.next = head;
+        return head;
+    }
 
     /**
      * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
@@ -143,9 +170,9 @@ public class MediumSolution {
      * 给定 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。
      * 在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。
      * 找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
-     *
+     * <p>
      * 图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为 49。
-     *
+     * <p>
      * 来源：力扣（LeetCode）
      * 链接：https://leetcode-cn.com/problems/container-with-most-water
      * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。

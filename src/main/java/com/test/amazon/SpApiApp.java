137c7e0e0b0737c469bccaebefae9a41132c2214
zhouyunxiang <zhouyunxiang@huice.com>
2022-09-07 14:03:44 +0800

    add;

--- src/main/java/com/test/amazon/SpApiApp.java
+++ src/main/java/com/test/amazon/SpApiApp.java
@@ -1,17 +1,5 @@
 package com.test.amazon;
 
-import cn.wangdian.amazon.SellingPartnerAPIAA.AWSAuthenticationCredentials;
-import cn.wangdian.amazon.SellingPartnerAPIAA.AWSAuthenticationCredentialsProvider;
-import cn.wangdian.amazon.SellingPartnerAPIAA.LWAAuthorizationCredentials;
-import cn.wangdian.amazon.spapi.solicitations.api.SolicitationsApi;
-import cn.wangdian.amazon.spapi.solicitations.invoker.ApiException;
-import cn.wangdian.amazon.spapi.solicitations.model.CreateProductReviewAndSellerFeedbackSolicitationResponse;
-import com.alibaba.fastjson.JSON;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class SpApiApp {
 
 }

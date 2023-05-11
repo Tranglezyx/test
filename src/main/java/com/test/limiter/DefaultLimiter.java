03b8098a9b6546ae884e6124d3ec4327af9eefcc
Trangle <407833@qq.com>
2023-05-11 19:44:59 +0800

    限流器调用;

--- src/main/java/com/test/limiter/DefaultLimiter.java
+++ src/main/java/com/test/limiter/DefaultLimiter.java
@@ -26,22 +26,20 @@ public class DefaultLimiter implements Limiter {
         long count = zset.stream().count();
         long time = LocalDateTime.now().toInstant(ZoneOffset.ofHours(8)).toEpochMilli();
         String key = UUID.randomUUID().toString();
-        if (count < configEnum.getBurst()) {
-            zset.add(time, key);
-            return true;
-        } else {
+        if (count >= configEnum.getBurst()) {
             Double score = zset.firstScore();
             BigDecimal firstTime = new BigDecimal(score);
             BigDecimal nowTime = new BigDecimal(time);
-            BigDecimal value = nowTime.subtract(firstTime).multiply(configEnum.getRate()).divide(new BigDecimal(1000),2, RoundingMode.HALF_UP);
+            // 当前时间距离第一个令牌调用时间间隔产生的令牌数量 = 时间间隔 * 令牌产生速率 / 1000
+            BigDecimal value = nowTime.subtract(firstTime).multiply(configEnum.getRate()).divide(new BigDecimal(1000), 2, RoundingMode.HALF_UP);
             if (value.compareTo(BigDecimal.ONE) < 0) {
                 return false;
             }
-            for (int i = 0; i < value.intValue(); i++) {
+            for (int i = 0; i < Math.min(value.intValue(), configEnum.getBurst()); i++) {
                 zset.pollFirst();
             }
-            zset.add(time, key);
-            return true;
         }
+        zset.add(time, key);
+        return true;
     }
 }

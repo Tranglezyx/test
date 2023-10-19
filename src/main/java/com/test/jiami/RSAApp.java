4d1b1cd7f4ebf108259976826181424d2b2a4c20
Trangle <407833@qq.com>
2023-10-19 15:59:00 +0800

    加解密;

--- src/main/java/com/test/jiami/RSAApp.java
+++ src/main/java/com/test/jiami/RSAApp.java
@@ -1,19 +1,56 @@
 package com.test.jiami;
 
 import cn.hutool.core.codec.Base64;
+import cn.hutool.crypto.Mode;
+import cn.hutool.crypto.Padding;
 import cn.hutool.crypto.SecureUtil;
 import cn.hutool.crypto.asymmetric.AsymmetricAlgorithm;
 import cn.hutool.crypto.asymmetric.KeyType;
 import cn.hutool.crypto.asymmetric.RSA;
+import cn.hutool.crypto.symmetric.AES;
 import lombok.extern.slf4j.Slf4j;
 
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
 import java.security.KeyPair;
 
 @Slf4j
 public class RSAApp {
 
     public static void main(String[] args) {
-        test();
+        decode();
+    }
+
+    public static void decode(){
+        String secretKey = "MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBANnOPp+W3MhQXdXm\n" +
+                "nrbj/EpgUB4FYbbuNzL+5Fjf4bfpedPO/bdf34M88IIi1RRiM9RtV6mMNXCu3/lo\n" +
+                "AL31wp5cUgoVl97XpPe74OYwbqdqrZYoyu0yCjKlturjPvYxSW5fV732Vrxk2v03\n" +
+                "CjQ6BT0dNigpERN0IIOiqVTMSKT3AgMBAAECgYA2eHQE6W/R0PeGtSRgTOgdmbSv\n" +
+                "rVzgqPa5FDH7YBJNMmC7bfrZz2pQof7sLNXpQR4BNE9GIcCDq9hrwWzeFQZUqlid\n" +
+                "9a62YwtNtAdxTwan1I89wFkJFkDD/5eG2+kr9SVK6kq3bqYP7PwBUFFkhkuRrbME\n" +
+                "Dlzl2uOKw55MXJKMoQJBAO50HP6MslX5DsFWiIVdbNSrgyo9hE/HSojbprORjW8t\n" +
+                "MBq6bJxqjkFdY/xOt9vvn8kweLQO8HvxMo5U/fgyHUkCQQDp1Syv4igTD4SmnaaC\n" +
+                "XTAKIuasWff3sXy9fhsdxvbjhBxJdlYcBAGbeqBQxpf0vx2K7cKtsQG0KBskvozz\n" +
+                "h/A/AkEAjuaA9b+FpGYkhJMCL8JkVTMP4QGKyHCrZ0cTKOvica0H9gygyEBHTj7U\n" +
+                "i8D1CFYbUOu+elG1puFOJeQja1lFGQJANr44kwjAVw4IATXhWJ3WdFkNZgY+HlgI\n" +
+                "Gxjd4k/FVyiDstMof7CUa8D+rUqBIsRndzIIpL9O5c2J5UVTHGST4wJBAJgL2bMq\n" +
+                "xI1os7Y/+erBQJN8qG+Gll9hIJaeRalPJNUdyjuLelsOtmgLxazjghYniSoEa+Dv\n" +
+                "LhBDhNMek6pUsng=";
+        String publicKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDZzj6fltzIUF3V5p624/xKYFAe\n" +
+                "BWG27jcy/uRY3+G36XnTzv23X9+DPPCCItUUYjPUbVepjDVwrt/5aAC99cKeXFIK\n" +
+                "FZfe16T3u+DmMG6naq2WKMrtMgoypbbq4z72MUluX1e99la8ZNr9Nwo0OgU9HTYo\n" +
+                "KRETdCCDoqlUzEik9wIDAQAB";
+        String randomKeyAes = "6530cee41cfac872";
+        String randomCode = "QegpuKn9EdeaolqHSFbqbbJQaZnNRuNhOB51Ka1HtTomOqk2Hdm8ULz8EyLiHRLWu+Vhz5Llt4W/WO7w0artd3BEFweFiSyHByQ3CWC44dm4SjZJdtzuReFAooWDN9mH7ByQZFJ72U6ySQHqlktIpWUwfvYLA19uzzeZshBQvC8=";
+        RSA rsa = new RSA(AsymmetricAlgorithm.RSA_ECB_PKCS1.getValue(), secretKey, publicKey);
+        String decryptStr = rsa.decryptStr(randomCode, KeyType.PrivateKey);
+        log.info("解密结果：{},{}", decryptStr,decryptStr.equals(randomKeyAes));
+
+        String phoneSecret = "W7HWMp0VDQoUYi/c17+Asg==";
+        AES aes = new AES(Mode.ECB, Padding.ZeroPadding, new SecretKeySpec(randomKeyAes.getBytes(), "AES"));
+
+        String decrypt = new String(aes.decrypt(phoneSecret), StandardCharsets.UTF_8);
+        log.info("解密结果：{}", decrypt);
     }
 
     public static void test() {
